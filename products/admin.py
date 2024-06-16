043cb12aa3979d64f5790e5ae3105487529dc24c
hossein <hossein.h.a770227@gmail.com>
2024-06-16 20:05:37 +0330

    add product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
+from .models import Prouduct
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Prouduct)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','active','date_time_created',]
--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Prouduct(models.Model):
+    name = models.CharField(_("name"), max_length=100)
+    description = models.TextField(_("description"))
+    short_description = models.CharField(_("short text"), max_length=200)
+    price = models.PositiveIntegerField(_("price"))
+    price_with_discount = models.PositiveIntegerField(_("price with discount"))
+    active = models.BooleanField(_("active"), default=True)
+    date_time_created = models.DateTimeField(_("date time created"), auto_now_add=True)
+    date_time_modified = models.DateTimeField(_("date time modified"), auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+

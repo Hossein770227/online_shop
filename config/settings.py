3956e5ced5868c802000d02d8dc7564954b210a4
hossein <hossein.h.a770227@gmail.com>
2024-06-16 15:47:12 +0330

    add products app

--- config/settings.py
+++ config/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my app
-    "accounts",
-    "pages",
+    'accounts',
+    'pages',
+    'products',
 ]
 
 MIDDLEWARE = [
